d7b0b29f005b2596d409752f8270d434fb2ee7ee
BT_1N3_28 <jjacobs8462@bths.edu>
2019-02-11 08:33:14 -0500

    added challenge 5

--- src/Thingy.java
+++ src/Thingy.java
@@ -1,10 +1,11 @@
-public class Thingy implements Comparable<Thingy> {
+public class Thingy {
     private int value;
     public Thingy(int value)
     {
-        this.value = value;
+        this.value = (int)(Math.random()*10000);
     }
 
+    @Override
     public int compareTo(Thingy other)
     {
         return this.value - other.value;
